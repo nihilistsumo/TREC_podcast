876c3e9937c081365ec0060530d2ba1e39a5251a
Sumanta  Kashyapi <sk1105@wildcats.unh.edu>
2020-08-29 00:18:31 -0400

    data handling for trec training data

--- summary_vec_gen.py
+++ summary_vec_gen.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import numpy as np
+import json
 
 class SummaryEmbedGen(nn.Module):
 
@@ -20,6 +21,52 @@ class SummaryEmbedGen(nn.Module):
         vec_out = self.lout(last_lstm_output)
         return vec_out
 
+def run_model(qrels_secid_data, secids, secid_vecs, paraids, paraid_vecs, max_seq_len, emb_vec_size):
+    X = []
+    y = []
+    for s in qrels_secid_data.keys():
+        paralist = qrels_secid_data[s]['paras']
+        if len(paralist) < 3:
+            continue
+        dat = []
+        for p in paralist:
+            paravec = paraid_vecs[paraids.index(p)]
+            dat.append(paravec)
+        if len(dat) > max_seq_len:
+            dat = dat[:max_seq_len]
+        elif len(dat) < max_seq_len:
+            dat += list(np.zeros((max_seq_len - len(dat), emb_vec_size)))
+        X.append(dat)
+        y.append(secid_vecs[secids.index(s)])
+    X = torch.tensor(X).float()
+    y = torch.tensor(y).float()
+
+    m = SummaryEmbedGen(emb_vec_size, max_seq_len)
+    opt = optim.Adam(m.parameters(), lr=0.001)
+    mseloss = nn.MSELoss()
+    for i in range(1000):
+        opt.zero_grad()
+        ypred = m(X)
+        loss = mseloss(ypred, y)
+        loss.backward()
+        opt.step()
+        if i % 100 == 0:
+            print(loss)
+    print(y)
+    print(m(X))
+
+
+def main():
+    with open('/media/sumanta/Seagate Backup Plus Drive/TREC2020_podcast_track/treccar_helper_data/by1train_nodup_toplevel_secid.json', 'r') as qd:
+        dat = json.load(qd)
+    secids = list(np.load('/media/sumanta/Seagate Backup Plus Drive/TREC2020_podcast_track/treccar_helper_data/by1train_toplevel_sections.npy'))
+    secid_vecs = np.load('/media/sumanta/Seagate Backup Plus Drive/TREC2020_podcast_track/treccar_helper_data/by1train_toplevel_section_vecs.npy')
+    paraids = list(np.load('/media/sumanta/Seagate Backup Plus Drive/SentenceBERT_embeddings/sentbert_embeddings_by1train/bert-base-passage-wiki-sec-mean/paraids.npy'))
+    paraid_vecs = np.load('/media/sumanta/Seagate Backup Plus Drive/SentenceBERT_embeddings/sentbert_embeddings_by1train/bert-base-passage-wiki-sec-mean/bert-base-wikipedia-sections-mean-tokens-passage-part1.npy')
+
+    run_model(dat, secids, secid_vecs, paraids, paraid_vecs, 10, 768)
+
+'''
 def main():
     sample_size = 25
     emb_size = 8
@@ -41,6 +88,7 @@ def main():
             print(loss)
     print(y)
     print(m(X))
+'''
 
 if __name__ == '__main__':
     main()
